06553cce8ad0f1d0150796b2e30a76a518543cf3
Chris Tully <christully12@gmail.com>
2025-06-04 14:31:53 -0400

    Buttons and anchors

--- react-ts-basic-input/src/App.tsx
+++ react-ts-basic-input/src/App.tsx
@@ -1,16 +1,26 @@
-import Input from "./components/Input.tsx";
+// import Input from "./components/Input.tsx";
+import Button from "./components/Button.tsx";
 
 function App() {
   return (
     <main>
-      <Input
+      <p>
+        {/* //Note: href doesn't work due to the el */}
+        <Button el="button"> A Button</Button>
+      </p>
+      <p>
+        <Button el="anchor" href="https://google.com">
+          A Link
+        </Button>
+      </p>
+      {/* <Input
         id="name"
         label="Your name"
         type="text"
         disabled
         aria-label="abc"
       />
-      <Input id="age" label="Your age" type="number" />
+      <Input id="age" label="Your age" type="number" /> */}
     </main>
   );
 }
