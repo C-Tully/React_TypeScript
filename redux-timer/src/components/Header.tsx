9187e2400aac43f6d8cb3190f18ebaa874bfe1fb
Chris Tully <christully12@gmail.com>
2025-06-09 12:42:19 -0400

    Updated timers dynamic state

--- redux-timer/src/components/Header.tsx
+++ redux-timer/src/components/Header.tsx
@@ -1,11 +1,18 @@
-import Button from './UI/Button.tsx';
+import { useContext } from "react";
+import { TimersContext, useTimersContext } from "../store/timers-context.tsx";
+import Button from "./UI/Button.tsx";
 
 export default function Header() {
+  const timersCtx = useTimersContext();
+  // const timersCtx = useContext(TimersContext)!;
+  // if(timersCtx === null)
+  //   throw new Error('Error!');
+
   return (
     <header>
       <h1>ReactTimer</h1>
 
-      <Button>Stop Timers</Button>
+      <Button>{timersCtx.isRunning ? "Stop" : "Start"} Timers</Button>
     </header>
   );
 }
--- redux-timer/src/store/timers-context.tsx
+++ redux-timer/src/store/timers-context.tsx
@@ -1,4 +1,4 @@
-import { createContext, type ReactNode } from "react";
+import { createContext, useContext, useReducer, type ReactNode } from "react";
 
 type Timer = {
   name: string;
@@ -17,24 +17,154 @@ type TimersContextValue = TimersState & {
   stopTimers: () => void;
 };
 
-const TimersContext = createContext<TimersContextValue | null>(null);
+export const TimersContext = createContext<TimersContextValue | null>(null);
+
+export function useTimersContext() {
+  const timersCtx = useContext(TimersContext);
+
+  if (timersCtx === null) throw new Error("Timers Context is null - error");
+
+  return timersCtx;
+}
 
 type TimersContextProviderProps = {
   children: ReactNode;
 };
+
+type StartTimersAction = {
+  type: "START_TIMERS";
+};
+type StopTimersAction = {
+  type: "STOP_TIMERS";
+};
+
+type AddTimerAction = {
+  type: "ADD_TIMER";
+  payload: Timer;
+};
+
+type Action = StartTimersAction | StopTimersAction | AddTimerAction;
+
+// type Action = {
+//   type: "ADD_TIMER" | "START_TIMERS" | "STOP_TIMERS";
+//   payload?: Timer;
+// };
+
+//marry the reducers state to the other applicale states. E.G initialState
+function timersReducer(state: TimersState, action: Action): TimersState {
+  //Define some code to handle the actions and change/produce state appropriatly
+  if (action.type === "START_TIMERS") {
+    return {
+      ...state,
+      isRunning: true,
+    };
+  }
+  if (action.type === "STOP_TIMERS") {
+    return {
+      ...state,
+      isRunning: false,
+    };
+  }
+  if (action.type === "ADD_TIMER") {
+    return {
+      ...state,
+      timers: [
+        ...state.timers,
+        {
+          name: action.payload!.name,
+          duration: action.payload.duration,
+        },
+      ],
+    };
+  }
+
+  return state;
+}
 export default function TimersContextProvider({
   children,
 }: TimersContextProviderProps) {
-  //accessing the provider property
-  //requires the 'value'
+  // could be useState...
+  // useReducer(reducerFunction, InitalStateToBeManaged);
+
+  const [timersState, dispatch] = useReducer(timersReducer, initialState);
   const ctx: TimersContextValue = {
-    timers: [],
-    isRunning: false,
-    addTimer(timerData) {},
-    startTimers() {},
-    stopTimers() {},
+    timers: timersState.timers,
+    isRunning: timersState.isRunning,
+    addTimer(timerData) {
+      dispatch({
+        type: "ADD_TIMER",
+        payload: timerData,
+      });
+    },
+    startTimers() {
+      dispatch({
+        type: "START_TIMERS",
+      });
+    },
+    stopTimers() {
+      dispatch({
+        type: "STOP_TIMERS",
+      });
+    },
   };
   return (
     <TimersContext.Provider value={ctx}>{children}</TimersContext.Provider>
   );
 }
+
+const initialState: TimersState = {
+  isRunning: true,
+  timers: [],
+};
+
+// Context
+// import { createContext, useContext, type ReactNode } from "react";
+
+// type Timer = {
+//   name: string;
+//   duration: number;
+// };
+
+// type TimersState = {
+//   isRunning: boolean;
+//   timers: Timer[];
+// };
+
+// type TimersContextValue = TimersState & {
+//   //exposed functions
+//   addTimer: (timerData: Timer) => void;
+//   startTimers: () => void;
+//   stopTimers: () => void;
+// };
+
+// export const TimersContext = createContext<TimersContextValue | null>(null);
+
+// //Custom Hooks require the 'use' in the names
+// // useLoremIpsum()
+// export function useTimersContext() {
+//   const timersCtx = useContext(TimersContext);
+
+//   if (timersCtx === null) throw new Error("Timers Context is null - error");
+
+//   return timersCtx;
+// }
+
+// type TimersContextProviderProps = {
+//   children: ReactNode;
+// };
+// export default function TimersContextProvider({
+//   children,
+// }: TimersContextProviderProps) {
+//   //accessing the provider property
+//   //requires the 'value'
+//   const ctx: TimersContextValue = {
+//     timers: [],
+//     isRunning: false,
+//     addTimer(timerData) {},
+//     startTimers() {},
+//     stopTimers() {},
+//   };
+//   return (
+//     <TimersContext.Provider value={ctx}>{children}</TimersContext.Provider>
+//   );
+// }
