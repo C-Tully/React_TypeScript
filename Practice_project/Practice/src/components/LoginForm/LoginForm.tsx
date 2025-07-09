6dd8005c46f0da85cd5a6a0ee912b5ff7a33176d
Chris Tully <christully12@gmail.com>
2025-07-09 15:04:40 -0400

    Adding in basic form structure for login

--- Practice_project/Practice/src/components/LoginForm/LoginForm.tsx
+++ Practice_project/Practice/src/components/LoginForm/LoginForm.tsx
@@ -1,3 +1,41 @@
+import { useRef } from "react";
+import Form, { FormHandle } from "../Form/Form.tsx";
+import Input from "../Input/Input.tsx";
+
 export default function LoginForm() {
-  return <form></form>;
+  const form = useRef<FormHandle>(null);
+
+  const inputConfig = [
+    {
+      name: "uName",
+      type: "text",
+      id: "user-name",
+    },
+    {
+      name: "uPass",
+      type: "text",
+      id: "user-pass",
+    },
+  ];
+
+  function handleLoginAttempt(data: unknown) {
+    const extractedData = data as {
+      uName: string;
+      uPass: string;
+    };
+
+    //do some checks here
+
+    return true;
+  }
+
+  return (
+    <>
+      <Form ref={form} onSave={handleLoginAttempt} id="login-form">
+        {inputConfig.map((item, index) => (
+          <Input key={index} label={item.name} id={item.id} type={item.type} />
+        ))}
+      </Form>
+    </>
+  );
 }
