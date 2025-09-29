4e140dc7111d743e09c3922e79fd32af328f5862
Chris Tully <christully12@gmail.com>
2025-09-29 11:47:31 -0400

    updating routes

--- Practice_project/laravel-backend/routes/api.php
+++ Practice_project/laravel-backend/routes/api.php
@@ -2,25 +2,34 @@
 
 use Illuminate\Support\Facades\Route;
 use Illuminate\Http\Request;
-use App\Http\Controller\PostController;
+// use App\Http\Controllers\PostController;
 
-Route::prefix('posts')->group(function () {
-  Route::get('/posts/get-top-posts', [PostController::class, 'topPosts']);
-  Route::get('/{id}', [PostController::class, 'show']);
-  // Route::post('/new', [PostController::class, 'newPost']);
-  // Route::post('/edit', [PostController::class, 'editPost']);
-  // Route::post('/delete', [PostController::class, 'deletePost']);
-});
+// Route::prefix('posts')->group(function () {
+//   Route::get('/posts/get-top-posts', [PostController::class, 'topPosts']);
+//   Route::get('/{id}', [PostController::class, 'show']);
+//   // Route::post('/new', [PostController::class, 'newPost']);
+//   // Route::post('/edit', [PostController::class, 'editPost']);
+//   // Route::post('/delete', [PostController::class, 'deletePost']);
+// });
 
-Route::prefix('users')->group(function () {  
-  Route::get('/{id}', [UserController::class, 'show']);
+// Route::prefix('users')->group(function () {  
+//   Route::get('/{id}', [UserController::class, 'show']);
 
-  // Route::post('/edit', [UserController::class, 'editUser']);
-  // Route::post('/delete', [UserController::class, 'deleteUser']);
-  // Route::post('/new', [UserController::class, 'newUser']);
+//   // Route::post('/edit', [UserController::class, 'editUser']);
+//   // Route::post('/delete', [UserController::class, 'deleteUser']);
+//   // Route::post('/new', [UserController::class, 'newUser']);
 
-  Route::post('/login', [UserController::class, 'loginUser']);
-  Route::post('/logout', [UserController::class, 'logOut']);
+//   Route::post('/login', [UserController::class, 'loginUser']);
+//   Route::post('/logout', [UserController::class, 'logOut']);
   
+// });
+
+//Testing route
+Route::get('/ping', function (Request $request) {
+  return response()->json([
+      'status' => 'ok',
+      'service' => 'laravel-api',
+      'time' => now(),
+  ]);
 });
 
